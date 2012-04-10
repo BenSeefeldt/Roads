4421fec517bf105621a96b99f332334974fa549d
Kevin <kahorton@csbsju.edu>
2012-04-10 12:49:23 -0500

    Signed-off-by: Kevin <kahorton@csbsju.edu>

--- Java/src/Cluster.java
+++ Java/src/Cluster.java
@@ -7,5 +7,51 @@
  *
  */
 public class Cluster {
+	/*
+	centroid	double[] 
+	data		double[][]
+	getCentriod()	double[]
+	setCentroid(double[] centroid)
+	getData()	double[][]
+	setData(double[][] data)
+	*/
+	/**
+	 * the data structure holding the values for centroid of the cluster
+	 */
+	private double[] centroid;
+	
+	/**
+	 * the data structure holding the data points in the cluster
+	 */
+	private double[][] data;
+
+	/**
+	 * @return the centroid
+	 */
+	public double[] getCentroid() {
+		return centroid;
+	}
+
+	/**
+	 * @param centroid the centroid to set
+	 */
+	private void setCentroid(double[] centroid) {
+		this.centroid = centroid;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public double[][] getData() {
+		return data;
+	}
+
+	/**
+	 * @param data the data to set
+	 */
+	private void setData(double[][] data) {
+		this.data = data;
+	}
+	
 
 }
---
+++ Java/src/Distance.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author kahorton
+ *
+ */
+public class Distance {
+	/*
+	euc(double[] a, double[] b)
+	cos(double[] a, double[] b)
+	*/
+	public static double euclidian(double[] a, double[] b){
+		double dist = 0;
+		return dist;
+	}
+	
+	public static double cosine(double[] a, double[] b){
+		double dist = 0;
+		return dist;
+	}
+	
+
+}
